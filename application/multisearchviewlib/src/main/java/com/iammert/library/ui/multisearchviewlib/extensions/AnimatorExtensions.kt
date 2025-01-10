a95b35a44bbde82ff90ac902fd829c8f3e6d7fab
kunjanrshah <kunjanrshah@gmail.com>
2025-01-10 07:35:09 +0530

    resolve smart search crash issue and dashboard grid gray color issue

--- application/multisearchviewlib/src/main/java/com/iammert/library/ui/multisearchviewlib/extensions/AnimatorExtensions.kt
+++ application/multisearchviewlib/src/main/java/com/iammert/library/ui/multisearchviewlib/extensions/AnimatorExtensions.kt
@@ -6,21 +6,11 @@ import com.iammert.library.ui.multisearchviewlib.helper.SimpleAnimationListener
 
 fun ValueAnimator.endListener(onAnimationEnd: ()->Unit){
     addListener(object : SimpleAnimationListener(){
-        override fun onAnimationRepeat(animation: Animator) {
-            TODO("Not yet implemented")
-        }
 
         override fun onAnimationEnd(animation: Animator) {
             super.onAnimationEnd(animation)
             onAnimationEnd.invoke()
         }
 
-        override fun onAnimationCancel(animation: Animator) {
-            TODO("Not yet implemented")
-        }
-
-        override fun onAnimationStart(animation: Animator) {
-            TODO("Not yet implemented")
-        }
     })
 }
